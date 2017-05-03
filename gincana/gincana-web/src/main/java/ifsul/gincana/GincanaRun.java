9ca8f26c08661f8d7731f3373198a66577645b62
Eliseu Daroit <eliseudaroitjr@gmail.com>
2017-05-03 00:26:51 -0300

    feat(run): adicionar classe de execução.

---
+++ gincana/gincana-web/src/main/java/ifsul/gincana/GincanaRun.java
@@ -0,0 +1,15 @@
+package ifsul.gincana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eliseu Daroit
+ */
+@SpringBootApplication
+public class GincanaRun {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(GincanaRun.class, args);
+    }
+}
