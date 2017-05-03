2c63d6e18022105f8e2445dd206eaa0c9d40043c
Eliseu Daroit <eliseudaroitjr@gmail.com>
2017-05-03 00:31:46 -0300

    feat(security): adicionar configuração de segurança no sistema.

---
+++ gincana/gincana-security/src/main/java/ifsul/gincana/security/configuration/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package ifsul.gincana.security.configuration;
+
+import ifsul.gincana.security.service.GincanaUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author Eliseu Daroit
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    GincanaUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(final HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").defaultSuccessUrl("/", true)
+                    .failureUrl("/login?error").permitAll()
+                .and().logout().logoutUrl("/logout").deleteCookies("JSESSIONID").permitAll()
+                .and().csrf().disable();
+    }
+
+    @Autowired
+    public void setDetailsService(final AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+}
