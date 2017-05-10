2647ea4c30b5ff5feac8414f99b1757ce90d3953
marcelohenrique180 <marcelohs180@gmail.com>
2017-05-10 20:35:24 -0300

    refactor(login-controller): especificar método HTTP.

--- gincana/gincana-web/src/main/java/ifsul/gincana/controllers/LoginController.java
+++ gincana/gincana-web/src/main/java/ifsul/gincana/controllers/LoginController.java
@@ -1,14 +1,14 @@
 package ifsul.gincana.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String login(Model model){
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(){
         return "login";
     }
 }
