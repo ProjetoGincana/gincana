c17270c3e1c56c9131e3352d7ee45e982be4ec94
marcelohenrique180 <marcelohs180@gmail.com>
2017-05-17 00:08:25 -0300

    feat(prova): criar lógica da seleção de prova.

--- gincana/gincana-web/src/main/resources/develop/js/modules/provas.js
+++ gincana/gincana-web/src/main/resources/develop/js/modules/provas.js
@@ -3,3 +3,16 @@
 */
 
 require('../../scss/provas.scss');
+
+const opcoes = $(".conteudo-provas-tabela tr");
+const campoEquipe = $(".conteudo-pontuar-form-prova");
+
+opcoes.on("click", e => {
+  const selected = $(e.currentTarget);
+  const id = selected.children("th").text();
+
+  opcoes.removeClass("selected");
+  selected.addClass("selected");
+  
+  campoEquipe.attr("value", id);
+})
