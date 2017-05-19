a70b17408aa3eb5e721363d9fb2247b8ccffd4fb
marcelohenrique180 <marcelohs180@gmail.com>
2017-05-19 01:11:17 -0300

    feature(provas): maxima pontuação definida pela prova.

--- gincana/gincana-web/src/main/resources/develop/js/modules/provas.js
+++ gincana/gincana-web/src/main/resources/develop/js/modules/provas.js
@@ -6,13 +6,16 @@ require('../../scss/provas.scss');
 
 const opcoes = $(".conteudo-provas-tabela tr");
 const campoEquipe = $(".conteudo-pontuar-form-prova");
+const camposNota = $(".conteudo-pontuar-form-tabela-input");
 
 opcoes.on("click", e => {
   const selected = $(e.currentTarget);
   const id = selected.children("th").text();
+  const valorTotal = selected.children(".valor-total").text();
 
   opcoes.removeClass("selected");
   selected.addClass("selected");
   
+  camposNota.attr("max", valorTotal);
   campoEquipe.attr("value", id);
 })
