ebfc23f32f2dafa1e48635d0c07740ca0cce1b89
marcelohenrique180 <marcelohs180@gmail.com>
2017-05-19 00:35:23 -0300

    refactor(service): sistema de pontuação.

--- gincana/gincana-service/src/main/java/ifsul/gincana/service/repositories/EquipeRepository.java
+++ gincana/gincana-service/src/main/java/ifsul/gincana/service/repositories/EquipeRepository.java
@@ -1,7 +1,6 @@
 package ifsul.gincana.service.repositories;
 
 import ifsul.gincana.entity.entities.Equipe;
-import ifsul.gincana.entity.entities.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,10 +11,9 @@ import org.springframework.data.repository.query.Param;
  */
 public interface EquipeRepository extends PagingAndSortingRepository<Equipe, Long>{
     
-    @Query("SELECT SUM(p.peso * n.valor / 10) FROM Nota AS n "
+    @Query("SELECT SUM(n.valor) FROM Nota AS n "
             + "INNER JOIN n.equipe e "
-            + "INNER JOIN n.prova p "
             + "WHERE e.id = :#{#equipe.id}")
-    Double getPontuacao(@Param("equipe") Equipe equipe);
+    Integer getPontuacao(@Param("equipe") Equipe equipe);
     
 }
--- gincana/gincana-service/src/main/java/ifsul/gincana/service/services/EquipeService.java
+++ gincana/gincana-service/src/main/java/ifsul/gincana/service/services/EquipeService.java
@@ -6,9 +6,7 @@
 package ifsul.gincana.service.services;
 
 import ifsul.gincana.entity.entities.Equipe;
-import ifsul.gincana.entity.entities.Prova;
 import ifsul.gincana.service.repositories.EquipeRepository;
-import ifsul.gincana.service.repositories.ProvaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,7 +22,7 @@ public class EquipeService {
     @Autowired
     EquipeRepository repository;
 
-    public Double getPontuacao(Equipe equipe){
+    public Integer getPontuacao(Equipe equipe){
         return repository.getPontuacao(equipe);
     }
 
