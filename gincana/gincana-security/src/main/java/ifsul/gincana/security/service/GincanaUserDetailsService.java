0eb0236f556fe4174e5615188669f523c071220e
marcelohenrique180 <marcelohs180@gmail.com>
2017-05-25 02:27:06 -0300

    feat(usuario): receber role correto.

--- gincana/gincana-security/src/main/java/ifsul/gincana/security/service/GincanaUserDetailsService.java
+++ gincana/gincana-security/src/main/java/ifsul/gincana/security/service/GincanaUserDetailsService.java
@@ -3,7 +3,12 @@ package ifsul.gincana.security.service;
 import ifsul.gincana.entity.entities.Usuario;
 import ifsul.gincana.entity.enumerations.Permissoes;
 import ifsul.gincana.service.repositories.UsuarioRepository;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -23,7 +28,11 @@ public class GincanaUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException(String.format("Usuario com e-mail: %s não foi encontrado.", username));
 
         Usuario usuario = repository.findByEmail(username);
-        return new UserDetails(usuario.getEmail(), usuario.getSenha(), Permissoes.valuesToList(), usuario.getNome());
+        Collection<GrantedAuthority> permissoes = new ArrayList();
+        String authority = usuario.getPermissao().getAuthority();
+        permissoes.add(new SimpleGrantedAuthority(authority));
+        
+        return new UserDetails(usuario.getEmail(), usuario.getSenha(), permissoes, usuario.getNome());
     }
 
 }
