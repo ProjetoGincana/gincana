a0cc8b65a3953285f129d1684b89664b26fcf57a
marcelohenrique180 <marcelohs180@gmail.com>
2017-05-29 00:55:41 -0300

    style(bem-naming): refatorar conforme convenção de nome BEM.
    Fica a critério da equipe adotar ou não. Deixo esse snippet de como seria a utilização.

--- gincana/gincana-web/src/main/java/ifsul/gincana/controllers/LoginController.java
+++ gincana/gincana-web/src/main/java/ifsul/gincana/controllers/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,5 +46,10 @@ public class LoginController {
         }
         
         return "login";
+    }
+    
+    @GetMapping("/cadastro")
+    public String cadastarPage(){
+        return "cadastrado";
     }
 }
